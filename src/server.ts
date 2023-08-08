1f0bc87fa020c4c7edc2f638d3c425ce31a903b8
fceja <cisco_ceja@icloud.com>
2023-08-08 11:26:22 -0700

    fix ts nodemon issues

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express"
+import express, { Response } from "express"
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (res: Response) => {
     res.send("hello from ts api")
 })
 
