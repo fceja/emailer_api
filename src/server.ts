d968dac98d38241ff38003604628b08f0e724c2f
fceja <cisco_ceja@icloud.com>
2023-08-09 07:57:01 -0700

    temporarily add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 import router from "./routes/routes";
 
 dotenv.config({ path: "configs/.env" });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
+
+
+// NOTE - will need to verify this works
+const allowedOrigins = [""];
+
+app.use(cors({
+  origin: allowedOrigins
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
