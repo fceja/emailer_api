1d00db34d15820cb1096cf8a5546f0f648985735
FC <cisco_ceja@icloud.com>
2023-08-09 09:13:26 -0700

    implemnt nodemailer to send emails

--- src/routes/emailRoutes.ts
+++ src/routes/emailRoutes.ts
@@ -4,31 +4,50 @@ import nodemailer, { SentMessageInfo } from "nodemailer";
 const emailRouter = express.Router();
 
 emailRouter.post("/send", (req: Request, res: Response) => {
+  // parse contact info from request body
+  const { contactName, contactEmail, contactEmailMessage } = req.body;
+
+  // - log into email service provider
+  // - this is what emailer_api will leverage for email funtions
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: process.env.ACCOUNT_EMAIL_SERVICE,
     auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.ACCOUNT_EMAIL_ADDRESS,
+      pass: process.env.ACCOUNT_EMAIL_PASSWORD,
     },
   });
-  const { name, email, message } = req.body;
 
+  // - intit mail options
+  // - email will be sent to vendor email, and will contain contact info
+  //   parsed from request body
   const mailOptions = {
-    to: "emailerapi61@gmail.com",
-    subject: "Marketing Lead",
+    to: process.env.VENDOR_EMAIL,
+    subject: process.env.VENDOR_EMAIL_SUBJECT,
+    text: `
+      Contact Info
+      - Name: ${contactName}
+      - Email: ${contactEmail}
+      - Message: ${contactEmailMessage}
 
-    text: `Name: ${name} \nEmail: ${email} \nMessage: ${message}`,
+
+      ------------------------
+      AUTOMATED EMAIL
+      ACCOUNT IS NOT MONITORED
+    `,
   };
 
+  // send email
   transporter.sendMail(
     mailOptions,
     (error: Error | null, info: SentMessageInfo) => {
       if (error) {
+        // email send error
         console.log(error);
-        res.status(500).send("Email could not be sent.");
+        res.status(500).send({ message: "Email could not be sent." });
       } else {
+        // email sent
         console.log("Email send: " + info.response);
-        res.send("Email sent successfully.");
+        res.send({ message: "Email sent successfully." });
       }
     }
   );
