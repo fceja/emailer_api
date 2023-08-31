b684a3f18999f3337e11d0797ca8c9687cca9a53
FC <cisco_ceja@icloud.com>
2023-08-31 10:24:57 -0700

    create aws dir, add generateEmailFunc lambda

---
+++ aws/lambdas/generateEmailFunc/dist/nodemailer.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.executeSendEmail = void 0;
+var nodemailer = require("nodemailer");
+// return transporter object with configs for email service provider
+var getTransporter = function () {
+    console.log("process.env.ACCOUNT_EMAIL_SERVICE", process.env.ACCOUNT_EMAIL_SERVICE);
+    console.log("process.env.ACCOUNT_EMAIL_ADDRESS", process.env.ACCOUNT_EMAIL_ADDRESS);
+    console.log("process.env.ACCOUNT_EMAIL_PASSWORD", process.env.ACCOUNT_EMAIL_PASSWORD);
+    return nodemailer.createTransport({
+        service: process.env.ACCOUNT_EMAIL_SERVICE,
+        auth: {
+            user: process.env.ACCOUNT_EMAIL_ADDRESS,
+            pass: process.env.ACCOUNT_EMAIL_PASSWORD,
+        },
+    });
+};
+// return object with email properties to be sent
+var getMailOptions = function (emailFormatStr) { return ({
+    to: process.env.VENDOR_EMAIL,
+    subject: process.env.VENDOR_EMAIL_SUBJECT,
+    text: "".concat(emailFormatStr),
+}); };
+// send email
+var sendEmail = function (mailOptions, transporter) {
+    return new Promise(function (resolve, reject) {
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                console.log("Email send: " + info.response);
+                resolve();
+            }
+        });
+    });
+};
+var executeSendEmail = function (emailFormatStr) {
+    // init objects to send email
+    var mailOptionsObj = getMailOptions(emailFormatStr);
+    console.log("mailOptionsObj", mailOptionsObj);
+    var transporterObj = getTransporter();
+    return sendEmail(mailOptionsObj, transporterObj);
+};
+exports.executeSendEmail = executeSendEmail;
---
+++ aws/lambdas/generateEmailFunc/index.ts
@@ -0,0 +1,38 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { executeSendEmail } from "./nodemailer";
+
+export const handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log("called exports handler");
+  try {
+    const req = event.body ? JSON.parse(event.body) : {} || {};
+    const { contactName, contactEmail, contactEmailMessage } = req.body;
+
+    // generate email format string with contact info
+    const emailFormatStr = `
+      Contact Info
+      - Name: ${contactName}
+      - Email: ${contactEmail}
+      - Message: ${contactEmailMessage}
+
+
+      ------------------------
+      AUTOMATED EMAIL
+      ACCOUNT IS NOT MONITORED
+    `;
+
+    // execute send email process
+    await executeSendEmail(emailFormatStr);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully." }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Email could not be sent." }),
+    };
+  }
+};
