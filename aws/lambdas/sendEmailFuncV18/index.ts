7064a4cd80a8d03fd1cefd833e0a78347049ea63
fceja <cisco_ceja@icloud.com>
2023-11-07 14:27:29 -0800

    re-add sendEmailFuncV18 lambda

---
+++ aws/lambdas/sendEmailFuncV18/index.ts
@@ -0,0 +1,50 @@
+import type { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { executeSendEmail } from "./lib/myNodemailer";
+
+const returnError = () => ({
+  statusCode: 500,
+  body: JSON.stringify({
+    message: "Email could not be sent.",
+  }),
+});
+
+exports.handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  const payload = event.body ? JSON.parse(event.body) : {} || {};
+  const { contactName, contactEmail, contactEmailMessage } = payload;
+
+  if (!contactName || !contactEmail || !contactEmailMessage) {
+    return returnError();
+  }
+
+  // generate email format string with contact info
+  const emailFormatStr = `
+    Contact Info
+    - Name: ${contactName}
+    - Email: ${contactEmail}
+    - Message: ${contactEmailMessage}
+
+
+    ------------------------
+    AUTOMATED EMAIL
+    ACCOUNT IS NOT MONITORED
+  `;
+
+  try {
+    // execute send email process
+    await executeSendEmail(emailFormatStr);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin":
+          `${process.env.ACCESS_CONTROL_ALLOW_ORIGIN}`,
+      },
+      body: JSON.stringify({ message: "Email sent successfully." }),
+    };
+  } catch (error) {
+    console.log("error", error);
+    return returnError();
+  }
+};
